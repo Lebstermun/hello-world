34013f10cac2d41cda8b3f625a0e6ea0253101fc
Will Harder <willharder1512@gmail.com>
2018-11-06 13:23:40 -0500

    JavaScript

---
+++ function.js
@@ -0,0 +1,17 @@
+function myconsole() {
+console.log("Hello, World!");
+}
+
+function popup() {
+  alert("Hello, World!");
+}
+
+function print() {
+  var p=document.getElementById("show");
+  p.innerHTML="Hello, World";
+}
+
+function stranger() {
+  var name=prompt("insert name");
+  alert("Hello " + name);
+}
